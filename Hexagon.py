de03c8e778d20922b3eeb12422bbae560d736b16
Ayush Gupta <ayush227316@gmail.com>
2022-02-22 09:19:29 +0530

    code

---
+++ Hexagon.py
@@ -0,0 +1,20 @@
+from tkinter import *
+import turtle
+
+colors = ['white', 'pink', 'violet', 'magenta', 'purple', 'indigo', '']  # declaring all the colors
+
+t = turtle.Pen()  # creating the object
+t.speed(10)
+
+turtle.bgcolor('black')  # background color
+
+for x in range(360):
+    t.pencolor(colors[x % 6])
+    t.width(int(x/100) + 1)
+    t.forward(x)
+    # print(x)
+    t.left(59)
+
+ts = turtle.getscreen()
+ts.getcanvas().postscript(file="duck4.eps")
+turtle.done()
---
+++ rose.py
@@ -0,0 +1,17 @@
+from tkinter import *
+import turtle
+colors = ['red', 'red', 'red', 'red', 'red', 'red']  # declaring all the colors
+
+t = turtle.Pen()  # creating the object
+t.speed(100)
+
+turtle.bgcolor('black')  # background color
+
+for x in range(360):
+    t.pencolor(colors[x % 6])
+    t.width(int(x/100) + 1)
+    t.forward(x)
+    t.left(49)
+    ts = turtle.getscreen()
+    ts.getcanvas().postscript(file="duck2.eps")
+turtle.done()
